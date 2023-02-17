f3b36c1fc625bcade2b4270b66cc1af0ad8f7afb
Mahdieh Shavandi <mdiehsh@gmail.com>
2023-02-17 23:07:05 +0330

    chore: favicon changes applied

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Youtumize" key="title"/>
-        <meta property="og:description" content="level-up your Youtube channel" key="description"/>
+        <meta property="og:title" content="Youtumize" key="title" />
+        <meta
+          property="og:description"
+          content="level-up your Youtube channel"
+          key="description"
+        />
         <meta
           property="og:image"
           content="<%= require('../assets/logo.png') %>"
         />
-        <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
